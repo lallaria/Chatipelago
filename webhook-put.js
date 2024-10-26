3dcb552ff00c222e076ac29f2fa81e0136894461
lallaria <lindsaydiane@gmail.com>
2024-10-25 21:33:19 -0400

    First checkin of STUFF

---
+++ webhook-put.js
@@ -0,0 +1,5 @@
+/*
+When the webserver receives something from AP, it should push to MixItUp (can also push to the obs-source carousel)
+https://wiki.mixitupapp.com/en/commands/webhook-commands
+
+*/
