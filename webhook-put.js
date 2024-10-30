b95c53d6aca4522a0c36d4467738361ee4bd1a9a
DESKTOP-5D0I04S\Dev <123@123.com>
2024-10-29 20:12:40 -0400

    Implement Webhook and achipelagoHelper (based on Clique logic)

--- webhook-put.js
+++ webhook-put.js
@@ -3,3 +3,26 @@ When the webserver receives something from AP, it should push to MixItUp (can al
 https://wiki.mixitupapp.com/en/commands/webhook-commands
 
 */
+
+var config = require("./config.js");
+
+module.exports = {
+    postInChat: function (message) {
+        console.log(`Posting "${message}" in chat`);
+        post(message, config.webhookUrl);
+    }
+}
+
+function post(message, url) {
+
+    var content = { text: message };
+
+    response = fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(content)
+    });
+
+}
