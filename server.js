d58b7f574d1a2223fff95022d9e8edc99635a333
DESKTOP-5D0I04S\Dev <123@123.com>
2024-10-29 20:12:40 -0400

    Implement Webhook and achipelagoHelper (based on Clique logic)

--- server.js
+++ server.js
@@ -4,13 +4,22 @@ https://www.npmjs.com/package/archipelago.js
 
 */
 
+var onEvent;
 
-
+module.exports = {
+    setOnEvent: function (fct) {
+        onEvent = fct;
+    }
+}
 'use strict';
 var http = require('http');
 var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
+    onEvent(req.url);
+
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
 }).listen(port);
+
+var bot = require('./bot-get.js');
