a2763b3e2385609e531f965ee4129122c7da1971
Dranzior <Math_lapierre@hotmail.com>
2024-11-03 19:37:49 -0500

    Added messageUtil and message pool for finding an item, failing to get the item, recieving an item from someone else, getting to a new location and whenever the cooldown end (#23)
    Co-authored-by: Dranzior <math_lapierre@hotmaill.com>

---
+++ messageUtil.js
@@ -0,0 +1,37 @@
+var fs = require('fs');
+
+function loadJson(filename) {
+    var json = JSON.parse(fs.readFileSync(filename, 'utf8'));
+    var messageList = [];
+
+    for (var i = 0; i < json.length; ++i) {
+        messageList[i] = json[i];
+    }
+
+    return messageList;
+}
+
+module.exports = {
+    generateRandomText: function (textList, variables) {
+        var randomPick = Math.floor(Math.random() * textList.length);
+
+        var text = textList[randomPick];
+        
+        if (variables != null) {
+            var keys = Object.keys(variables);
+            for (var i = 0; i < keys.length; ++i) {
+                var key = keys[i];
+                var value = variables[keys[i]];
+                text = text.replace(`{${key}}`, value);
+            }
+        }
+        
+        return text;
+    },
+
+    OFF_COOLDOWN: loadJson('messages/offCooldown.json'),
+    LOCATION_FOUND: loadJson('messages/locationFound.json'),
+    ITEM_MISSED: loadJson('messages/itemMissed.json'),
+    ITEM_FOUND: loadJson('messages/itemFound.json'),
+    ITEM_RECIEVED: loadJson('messages/itemRecieved.json')
+}
