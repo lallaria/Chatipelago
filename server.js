ab9551e5963abbaad2ee1e66c84a30b0c5ec6f0c
Dranzior <math_lapierre@hotmaill.com>
2025-03-18 21:19:02 -0400

    Added a barebone "webdebugger"

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import * as http from 'http';
+import fs from 'fs';
 
 export { setOnEvent, sayGoodBye }
 
@@ -17,10 +18,18 @@ function sayGoodBye() {
 var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
-    onEvent(req.url);
-
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, World\n');
+    if (req.url === '/home') {
+        fs.readFile('home.html', function(err, data) {
+            res.writeHead(200, {'Content-Type': 'text/html','Content-Length': data.length});
+            res.write(data);
+            res.end();
+        });
+    }
+    else {
+        onEvent(req.url);
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello, World\n');
+    }
 }).listen(port);
 
 import './bot-get.js'
